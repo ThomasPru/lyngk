e41250d732df251910bb6d75789833c86e9992c2
tprunier <thomas.23@live.fr>
2017-10-02 11:56:25 +0200

    Ajout du test de la question 2 ( fail )

--- test/test.js
+++ test/test.js
@@ -3,6 +3,11 @@
 var LyngkTestCase = TestCase("LyngkTestCase");
 
 LyngkTestCase.prototype.testA = function(){
-    var c = new Lyngk.Coordinates("A",1);
+    var c = new Lyngk.Coordinates("A",3);
     assertFalse(c.valid());
+}
+
+LyngkTestCase.prototype.testB = function(){
+    var c = new Lyngk.Engine();
+    assertTrue(c.validNbCasesValid());
 }
