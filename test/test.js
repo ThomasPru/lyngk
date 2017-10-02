2c93511fe33caa8111e96dd2dbeb3c5c98cff9d0
tprunier <thomas.23@live.fr>
2017-10-02 10:38:01 +0200

    Ajout du test (fail) de la question 1

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.testA = function(){
+    var c = new Lyngk.Coordinates("A",1);
+    assertFalse(c.valid());
+}
