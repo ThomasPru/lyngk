724dab7e790168c2641381c8a83bb0ef31e4f6af
tprunier <thomas.23@live.fr>
2017-10-09 09:24:09 +0200

    Ajout de la fonction du test 2 ( barre verte ) (fonctionne placé dans coordinates au lieu de engine)

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -17,8 +17,8 @@ Lyngk.Coordinates = function (co, li) {
         return array.indexOf([c.charCodeAt() - 65, l]) >= 0;
     }
 
-    /*
-    this.validNbCasesValid() = function(){
+
+    this.validNbCasesValid = function(){
         var array = [[1, 3],
             [2, 2], [2, 3], [2, 4], [2, 5],
             [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7],
@@ -28,13 +28,22 @@ Lyngk.Coordinates = function (co, li) {
             [7, 3], [7, 4], [7, 5], [7, 6], [7, 7], [7, 8], [7, 9],
             [8, 5], [8, 6], [8, 7], [8, 8],
             [9, 7]];
-        var counter = number(0);
-        var i=number(0);
+        var counter = 0;
+        var i=0;
+        var j=0;
         for (i in 9){
             for(j in 9){
+                if(array.indexOf([i, j]>=0)){
+                    counter++;
+                }
+            }
         }
+        if(counter===43){
+            return true;
+        }
+        return false;
     }
-    */
+
 
 };
 
