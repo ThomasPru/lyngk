cc1adb1fd315c8d3731ada34886e58d2998d3d3a
tprunier <thomas.23@live.fr>
2017-10-09 11:27:37 +0200

    Ajout de la fonction du test 7 ( barre verte )

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,9 +2,6 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
-    if(c==null){
-        c=0;
-    }
-    this.State=c;
+Lyngk.Intersection = function () {
+    this.State=0;
 };
