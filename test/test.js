1cc3406b1c56cf3250607a60d31af47c917e278c
tprunier <thomas.23@live.fr>
2017-10-09 10:16:58 +0200

    Ajout de la fonction du test 2 ( barre verte ) (fonctionne placé dans coordinates au lieu de engine)

--- test/test.js
+++ test/test.js
@@ -3,16 +3,30 @@
 var LyngkTestCase = TestCase("LyngkTestCase");
 
 LyngkTestCase.prototype.testA = function(){
-    var c = new Lyngk.Coordinates("A",3);
+    var c = new Lyngk.Coordinates("A",1);
     assertFalse(c.valid());
 }
 
 LyngkTestCase.prototype.testB = function(){
-    var c = new Lyngk.Coordinates();
-    assertFalse(c.validNbCasesValid());
+
+    var compteur=0;
+    var lettre = ['A','B','C','D','E','F','G','H','I'];
+
+    for(var j=0;j<lettre.length;j++) {
+        for (var i = 1; i < 10; i++) {
+            var co = new Lyngk.Coordinates(lettre[j],i);
+            if(co.valid()){
+                compteur++;
+            }
+        }
+    }
+    console.log(compteur);
+    assertEquals(compteur,43);
 }
 
+/*
 LyngkTestCase.prototype.testC = function(){
-    var c = new Lyngk.Coordinates();
+    var c = new Lyngk.Coordinates("B",4);
     assertFalse(c.CoordinRepresentation());
 }
+    */
