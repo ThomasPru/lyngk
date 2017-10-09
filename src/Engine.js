53273d3cde77fbc9d5078dc5021831bf4133f2d8
tprunier <thomas.23@live.fr>
2017-10-09 12:02:37 +0200

    Ajout de la fonction du test 8 ( barre rouge fail)

--- src/Engine.js
+++ src/Engine.js
@@ -3,5 +3,7 @@
 // enums definition
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
 
-Lyngk.Engine = function () {
+Lyngk.Engine = function (c) {
+    var couleur=c;
+    
 };
