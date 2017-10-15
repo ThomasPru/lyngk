eab2b92e8cd7340034e6daf55eb1dcb26751e6ac
eliroas <darktholliez@gmail.fr>
2017-10-15 14:09:01 +0200

    Ajout test 12(barre rouge fail)

--- test/test.js
+++ test/test.js
@@ -79,12 +79,70 @@ LyngkTestCase.prototype.testHist10 = function(){
 //histoire 11
 LyngkTestCase.prototype.testHist11 = function () {
     var jeu = new Lyngk.Engine();
+    jeu.Init_plateau_One();
     var compteur_one_piece=0;
-    for(var i=0;i<jeu.plateau.size();i++) {
-        jeu.plateau[i].poserPiece(Lyngk.Color.BLUE);
-        if (jeu.plateau[i].getState() == Lyngk.State.ONE_PIECE){
+
+    for(var i=0;i<jeu.getSizePlat();i++){
+        if(jeu.getPlateauETATindice(i)=== Lyngk.State.ONE_PIECE) {
             compteur_one_piece++;
         }
     }
-    assertTrue(compteur_one_piece===43);
+
+    assertEquals(compteur_one_piece,43);
+}
+
+
+//histoire 12
+LyngkTestCase.prototype.testHist12 = function(){
+    var jeu = new Lyngk.Engine();
+    jeu.Init_plateau_FULL();
+    var compteur_taille_inters_juste=0;
+
+    var cpt_ivoire=0;
+    var cpt_bleu=0;
+    var cpt_rouge=0;
+    var cpt_noir=0;
+    var cpt_vert=0;
+
+    var cpt_blanc=0;
+
+    var validNbPieceColo=0;
+    for(var i=0;i<jeu.getSizePlat();i++){
+        if(jeu.getTaillePileOnInter(i)=== 43) {
+            compteur_taille_inters_juste++;
+        }
+
+        //BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5
+        for (var j = 0; j < 43; j++) {
+            if(jeu.getCouleurFromPieceFromInterS(i,j) == Lyngk.Color.IVORY){
+                cpt_ivoire++;
+            }
+            if(jeu.getCouleurFromPieceFromInterS(i,j) == Lyngk.Color.BLUE){
+                cpt_bleu++;
+            }
+            if(jeu.getCouleurFromPieceFromInterS(i,j) == Lyngk.Color.RED){
+                cpt_rouge++;
+            }
+            if(jeu.getCouleurFromPieceFromInterS(i,j) == Lyngk.Color.BLACK){
+                cpt_noir++;
+            }
+            if(jeu.getCouleurFromPieceFromInterS(i,j) == Lyngk.Color.GREEN){
+                cpt_vert++;
+            }
+            if(jeu.getCouleurFromPieceFromInterS(i,j) == Lyngk.Color.WHITE){
+                cpt_blanc++;
+            }
+        }
+
+        if(cpt_ivoire==8 && cpt_bleu==8 && cpt_rouge==8 && cpt_noir==8 && cpt_vert==8 && cpt_blanc==3){
+            validNbPieceColo++;
+        }
+        cpt_ivoire=0;
+        cpt_bleu=0;
+        cpt_rouge=0;
+        cpt_noir=0;
+        cpt_vert=0;
+        cpt_blanc=0;
+    }
+    assertTrue(compteur_taille_inters_juste === 43 && validNbPieceColo===43);
 }
