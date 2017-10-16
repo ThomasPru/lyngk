b98fc16b43888df886a5960843b0c8e7861471e5
tprunier <thomas.23@live.fr>
2017-10-16 11:30:29 +0200

    Histoire 14 ( barre rouge FAIL 2)  + Correction histoire 12 (barre verte)

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,47 +2,44 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
-    var State= Lyngk.State.VACANT;
+Lyngk.Intersection = function () {
+    var state= Lyngk.State.VACANT;
     var piece;
     var couleurAssociee;
 
     var pile=[];
 
     this.getState = function () {
-        return State;
-    }
+        return state;
+    };
 
-    this.getPieceColorFromPile = function(i){
-        return pile[i].getCouleur();
-    }
 
     this.getCouleurAssociee = function(){
         return couleurAssociee;
-    }
+    };
 
     this.getTaillePile = function(){
         return pile.length;
-    }
+    };
+
 
     this.poserPiece = function(couleur){
         piece = new Lyngk.Piece(couleur);
         pile.push(piece);
         couleurAssociee = piece.getCouleur();
         if(pile.length===1){
-            State = Lyngk.State.ONE_PIECE;
+            state = Lyngk.State.ONE_PIECE;
         }
         else{
             if(pile.length>1 && pile.length<5) {
-                State = Lyngk.State.STACK;
+                state = Lyngk.State.STACK;
             }
             else{
                 if(pile.length>=5){
-                    State = Lyngk.State.FULL_STACK;
+                    state = Lyngk.State.FULL_STACK;
                 }
             }
         }
 
-    }
-
+    };
 };
